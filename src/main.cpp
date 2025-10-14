04e5af25127d022613131942aeaccd59a8acc903
randomman552 <random.man552@protonmail.com>
2025-10-14 22:24:30 +0100

    Add test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,41 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const uint8_t LEFT_UP_PIN = 11;
+const uint8_t LEFT_DOWN_PIN = 10;
+const uint8_t RIGHT_UP_PIN = 9;
+const uint8_t RIGHT_DOWN_PIN = 8;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Configure IO
+  pinMode(LEFT_UP_PIN, OUTPUT);
+  pinMode(LEFT_DOWN_PIN, OUTPUT);
+  pinMode(RIGHT_UP_PIN, OUTPUT);
+  pinMode(RIGHT_DOWN_PIN, OUTPUT);
+
+  // Start serial
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-}
+  digitalWrite(LEFT_UP_PIN, HIGH);
+  Serial.println("LEFT UP");
+  delay(1000);
+  digitalWrite(LEFT_UP_PIN, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  digitalWrite(RIGHT_UP_PIN, HIGH);
+  Serial.println("RIGHT UP");
+  delay(1000);
+  digitalWrite(RIGHT_UP_PIN, LOW);
+
+  digitalWrite(LEFT_DOWN_PIN, HIGH);
+  Serial.println("LEFT DOWN");
+  delay(1000);
+  digitalWrite(LEFT_DOWN_PIN, LOW);
+
+  digitalWrite(RIGHT_DOWN_PIN, HIGH);
+  Serial.println("RIGHT DOWN");
+  delay(1000);
+  digitalWrite(RIGHT_DOWN_PIN, LOW);
 }
