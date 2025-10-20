a1f7e5f536d0295611f9e550c007dd1b095ac1f1
randomman552 <random.man552@protonmail.com>
2025-10-20 22:17:33 +0100

    Add main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
 #include <Popup.h>
+#include <InputManager.h>
+#include <Animation.h>
+#include <AnimationTree.h>
 
+#define INPUT_PIN 12
+#define INPUT_DELAY 300
 #define LEFT_UP_PIN 11
 #define LEFT_DOWN_PIN 10
 #define RIGHT_UP_PIN 9
 #define RIGHT_DOWN_PIN 8
 
 // Create IO
+DigitalIO inputIO(INPUT_PIN);
 DigitalIO leftUpIO(LEFT_UP_PIN);
 DigitalIO leftDownIO(LEFT_DOWN_PIN);
 DigitalIO rightUpIO(RIGHT_UP_PIN);
@@ -15,13 +21,26 @@ DigitalIO rightDownIO(RIGHT_DOWN_PIN);
 // Create hardware control objects
 Popup leftPopup(&leftUpIO, &leftDownIO);
 Popup rightPopup(&rightUpIO, &rightDownIO);
+InputManager inputManager(&inputIO, INPUT_DELAY);
+
+// Create animations
+UpAnimation upAnimation(&leftPopup, &rightPopup);
+DownAnimation downAnimation(&leftPopup, &rightPopup);
+
+// Create animation tree
+AnimationTreeNode upNode(&upAnimation);
+AnimationTreeNode downNode(&downAnimation);
+AnimationTreeNode root(&upNode, &downNode);
 
 void setup()
 {
+  // Setup hardware
+  inputIO.setup();
   leftUpIO.setup();
   leftDownIO.setup();
   rightUpIO.setup();
   rightDownIO.setup();
+  inputManager.setup();
 
   // Start serial
   Serial.begin(9600);
@@ -29,4 +48,15 @@ void setup()
 
 void loop()
 {
+  // Poll inputs for input sequence
+  auto tick = millis();
+  auto sequence = inputManager.check(tick);
+
+  // If set, perform animation and clear sequence
+  if (sequence != nullptr)
+  {
+    auto animation = root.find(sequence);
+    animation->animate();
+    inputManager.clear();
+  }
 }
